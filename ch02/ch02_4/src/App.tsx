7ed787ed5ed549e4ff59bb2610d2efc5c617996d
tnwns <tnwnsghkd@naver.com>
2024-03-22 14:51:50 +0900

    chpater2

---
+++ ch02/ch02_4/src/App.tsx
@@ -0,0 +1,5 @@
+import P from './P'
+export default function App() {
+  const texts = ['hello', 'world'].map((text, index) => <P key={index} children={text} />)
+  return <div children={texts} />
+}
---
+++ ch02/ch02_4/src/P.tsx
@@ -0,0 +1,6 @@
+import type {FC, PropsWithChildren} from 'react'
+export type PProps = {}
+const P: FC<PropsWithChildren<PProps>> = props => {
+  return <p {...props} />
+}
+export default P
